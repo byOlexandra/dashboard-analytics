2edc63de9899585f867162ae1e716768ce397f38
devolexa <starovatovasasha@gmail.com>
2025-11-12 18:51:11 +0200

    update

--- src/js/main.js
+++ src/js/main.js
@@ -3,15 +3,18 @@ const refs = {
 }
 
 if (localStorage.getItem("theme") === "dark") {
-    document.body.classList.add("dark")
-}
+    document.body.classList.add("dark");
+    refs.toggleThemeBtn.textContent = "Light";
+} 
 
 refs.toggleThemeBtn.addEventListener("click", e => {
     e.preventDefault()
     document.body.classList.toggle("dark");
     if (document.body.classList.contains("dark")) {
-    localStorage.setItem("theme", "dark");
+        localStorage.setItem("theme", "dark");
+        refs.toggleThemeBtn.textContent = "Light";
     } else {
-    localStorage.setItem("theme", "light");
+        localStorage.setItem("theme", "light");
+        refs.toggleThemeBtn.textContent = "Dark";
     }
 })
